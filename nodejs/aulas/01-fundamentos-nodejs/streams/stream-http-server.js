ebbfb5f0747ea00fa6e0fb5bf0ea6539e85d8b2e
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-09-10 22:42:24 -0300

    Fundamentos de Nodejs

---
+++ nodejs/aulas/01-fundamentos-nodejs/streams/stream-http-server.js
@@ -0,0 +1,31 @@
+import http from 'node:http'
+import { Transform } from 'node:stream'
+
+class InverseNumberStream extends Transform {
+    _transform(chunck, encoding, callback) {
+        const transformed = Number(chunck.toString()) * -1
+
+        console.log(transformed)
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+
+// req => ReadableStream
+// res => WritableStream
+
+const server = http.createServer(async (req, res) => {
+
+    const buffers = []
+
+    for await (const chunck of req) {
+        buffers.push(chunck)
+    }
+
+    const fullStreamContent = Buffer.concat(buffers).toString()
+
+    return res.end(fullStreamContent)
+})
+
+server.listen(3334)
