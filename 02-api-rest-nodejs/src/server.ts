88d1de23f4c1ac036ce959b69147d9db95ead154
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-10-17 11:17:57 -0300

    Configurando o knex

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,11 +1,14 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
 // http://localhost:3333/hello
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
