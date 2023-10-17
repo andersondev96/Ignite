21681a8d59a3f6acc3486f276fbcfd432a7031e6
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-10-16 23:18:42 -0300

    Configurando o ESLint

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,15 +1,17 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 // http://localhost:3333/hello
 
 app.get('/hello', () => {
-    return 'Hello World'
+  return 'Hello World'
 })
 
-app.listen({
-    port: 3333
-}).then(() => {
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
     console.log('HTTP Server Running!')
-})
+  })
