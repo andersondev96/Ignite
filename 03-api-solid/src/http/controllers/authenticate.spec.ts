67fb72af9f05f3c8a27d4eb548f1c671fe7ab20f
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-11-02 20:18:40 -0300

    Testes E2E de autenticação

--- 03-api-solid/src/http/controllers/authenticate.spec.ts
+++ 03-api-solid/src/http/controllers/authenticate.spec.ts
@@ -2,7 +2,7 @@ import { app } from '@/app'
 import request from 'supertest'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-describe('Register (e2e)', () => {
+describe('Authenticate (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -11,13 +11,21 @@ describe('Register (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to register', async () => {
-    const response = await request(app.server).post('/users').send({
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
     })
 
-    expect(response.statusCode).toEqual(201)
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
   })
 })
