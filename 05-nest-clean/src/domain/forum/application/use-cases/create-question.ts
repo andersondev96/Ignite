8383b6093fca580b47adb74fc7ea06a2a513dfd1
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-12-29 21:40:34 -0300

    Comunicação entre camadas

--- 05-nest-clean/src/domain/forum/application/use-cases/create-question.ts
+++ 05-nest-clean/src/domain/forum/application/use-cases/create-question.ts
@@ -1,5 +1,6 @@
 import { Either, right } from '@/core/either'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Injectable } from '@nestjs/common'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list'
@@ -18,7 +19,7 @@ type CreateQuestionUseCaseResponse = Either<
     question: Question
   }
 >
-
+@Injectable()
 export class CreateQuestionUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
