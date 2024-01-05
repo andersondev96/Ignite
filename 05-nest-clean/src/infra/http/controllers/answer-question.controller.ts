e8a80b1eb059bad78bd0a3c300290a43a0ca936c
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-01-04 21:57:57 -0300

    Controller: Responder Pergunta

---
+++ 05-nest-clean/src/infra/http/controllers/answer-question.controller.ts
@@ -0,0 +1,45 @@
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Post,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+
+const answerQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(answerQuestionBodySchema)
+
+type answerQuestionBodySchema = z.infer<typeof answerQuestionBodySchema>
+@Controller('/questions/:questionId/answers')
+export class AnswerQuestionController {
+  constructor(private answerQuestion: AnswerQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: answerQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('questionId') questionId: string,
+  ) {
+    const { content } = body
+    const userId = user.sub
+
+    const result = await this.answerQuestion.execute({
+      content,
+      questionId,
+      authorId: userId,
+      attachmentsIds: [],
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
